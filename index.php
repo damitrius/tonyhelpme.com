839f596554c27c1a0690d1ef099393628ef35c8f
damitrius <trash@tjsites.net>
2019-05-23 20:55:47 -0400

    Upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html><head><title>Tony Help Me!</title><style>body {background-color: black;}</style></head>
+<body>
+<div><p> </p></div>
+<center>
+<div><p> </p></div>
+<div style="position:relative; width:234px; height:60px; margin-top: 50px;">
+  <a href="https://download.teamviewer.com/download/TeamViewerQS_en.exe" style="text-decoration:none;">
+    <img src="images/blue.png" alt="TeamViewer Windows" title="TeamViewer Windows" border="0" width="234" height="60" />
+    <span style="position:absolute; top:22.5px; left:55px; display:block; cursor:pointer; color:White; font-family:Arial; font-size:13px; line-height:1.2em; font-weight:bold; text-align:center; width:174px;">
+      TeamViewer Windows
+    </span>
+  </a>
+</div>
+
+
+<div><p> </p></div>
+
+<div style="position:relative; width:234px; height:60px; margin-top: 100px;">
+  <a href="https://download.teamviewer.com/download/TeamViewerQS.dmg" style="text-decoration:none;">
+    <img src="images/gray.png" alt="TeamViewer Mac" title="TeamViewer Mac" border="0" width="234" height="60" />
+    <span style="position:absolute; top:22.5px; left:55px; display:block; cursor:pointer; color:#424242; font-family:Arial; font-size:13px; line-height:1.2em; font-weight:bold; text-align:center; width:174px;">
+      TeamViewer Mac
+    </span>
+  </a>
+</div>
+
+
+<div><p> </p></div>
+
+<div style="position:relative; width:234px; height:60px; margin-top: 100px;">
+  <a href="https://chrome.google.com/webstore/detail/teamviewer/oooiobdokpcfdlahlmcddobejikcmkfo" style="text-decoration:none;">
+    <img src="images/gray.png" alt="TeamViewer ChromeOS" title="TeamViewer ChromeOS" border="0" width="234" height="60" />
+    <span style="position:absolute; top:22.5px; left:55px; display:block; cursor:pointer; color:#424242; font-family:Arial; font-size:13px; line-height:1.2em; font-weight:bold; text-align:center; width:174px;">
+      TeamViewer ChromeOS
+    </span>
+  </a>
+</div>
+</center>
+</body>
+</html>
